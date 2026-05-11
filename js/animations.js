800f770d6c0a9fcb09e9e95b661ee1e8299c51d3
Johannes Roth <jrothx@icloud.com>
2026-05-11 15:25:39 +0200

    first commit

---
+++ js/animations.js
@@ -0,0 +1,12 @@
+function animateTitle() {
+    const title = document.getElementById('title');
+    const content = document.getElementById('content');
+
+    setTimeout(() => {
+        title.classList.add('slide')
+    }, 1000);
+    setTimeout(() => {
+        content.classList.add('show');
+    }, 6000);
+
+}
